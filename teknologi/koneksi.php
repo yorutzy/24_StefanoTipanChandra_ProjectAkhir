47c10707c313493759639dd792554544954fabfb
yorutzy <stefanochandra82@gmail.com>
2025-11-17 09:38:13 +0700

    Delete teknologi directory

--- teknologi/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "mysql", "teknologi");
-echo('');
-if(mysqli_connect_error()){
-    echo "Koneksi database gagal : " . mysqli_connect_error();
-}
-?>
--- teknologi/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo "<script>alert('Anda telah logout!'); window.location='login.php';</script>";
-?>
